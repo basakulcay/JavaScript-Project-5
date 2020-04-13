cc6644a5c791e0e519ae0190a573f21670aca56e
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2020-04-13 17:31:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget=()=> {
+return Math.floor(Math.random()*10);}
+
+const compareGuesses=(humanGuess,computerGuess,targetGuess) => {
+if (Math.abs(humanGuess-targetGuess)<Math.abs(computerGuess-targetGuess)) {return true;}
+else if (Math.abs(humanGuess-targetGuess)>Math.abs(computerGuess-targetGuess)) {return false;}
+else {return true;};}
+
+const updateScore = winner => {
+  if (winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer') {
+    computerScore++;
+  }
+}
+
+const advanceRound = () => currentRoundNumber++;
+
+
